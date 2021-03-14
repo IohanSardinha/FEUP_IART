a9c72653c134730a9597f7e74790a59cd181134a
IohanSardinha <ioxsardinha@gmail.com>
2021-03-14 17:22:56 +0000

    Switch to pygame

--- graphics.py
+++ graphics.py
@@ -1,39 +1,49 @@
-from tkinter import *
+import pygame
 from tenpair import *
 
-piecePosition = (-1,-1)
-
-def clearButtons(buttons):
-    for line in buttons:
-        for button in line:
-            button.grid_forget()
+selectedPiece = (-1,-1)
 
 
+def drawBoard(board, screen, fontsmall, fontbig, moves):
+    startx, starty = (15,40)
 
-def buttonClick(position, button):
-    global piecePosition
+    screen.blit( fontsmall.render("Moves: "+str(moves), False, (255,255,255)) ,(startx, 10))
 
-    if position == piecePosition:
-        position = (-1,-1)
-        button.configure(bg = "gray")
-    elif not piecePosition == (-1,-1):
-        position = piecePosition
-        button.configure(bg = "yellow")
-    else:
-        print(piecePosition,position)
-
-def displayBoard(board):
-    buttons = [[0]*len(board[0]) for _ in board]
+    numbers = []
     for i, line in enumerate(board):
         for j, value in enumerate(line):
-            buttons[i][j] = Button( text = (" " if value == 0 else value),
-                                    command = lambda : buttonClick((i,j),self))
-            buttons[i][j].grid(row = i, column = j, padx = 2, pady = 2)    
-    Button(text = "deal").grid(column = len(board[0]), pady=5)
-    return buttons
+            screen.blit( fontbig.render("0" if value == 0 else str(value), False, (255,255,0) if selectedPiece == (i,j) else (255,255,255)) ,(startx+ j*30 + 8, starty + i*30 - 3))
+            number = pygame.draw.rect(screen, (255,255,0) if selectedPiece == (i,j) else (255,255,255), pygame.Rect(startx+ j*30, starty+ i*30, 30,30),1)
+            numbers.append((number, i, j, value))
+    return numbers
+
+pygame.init()
+pygame.font.init()
+screen = pygame.display.set_mode((300,600))
+
+clock = pygame.time.Clock()
+
+running = True
 
 board, moves = initialState()
-screen = Tk()
 
-displayBoard(board)
+fontsmall = pygame.font.SysFont('Arial', 20)
+fontbig = pygame.font.SysFont('Arial', 30)
 
+while running:
+    screen.fill((0,0,0))
+
+    numbers = drawBoard(board, screen, fontsmall, fontbig, moves)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mousePos = pygame.mouse.get_pos()
+            for number, i, j ,value in numbers:
+                if number.collidepoint(mousePos):
+                    print(i,j,value)
+
+    pygame.display.update()
+    clock.tick(60)
+pygame.quit()
