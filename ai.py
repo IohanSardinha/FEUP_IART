7eb07827ad6a34c38a99f05252f68c4bca6d338a
IohanSardinha <ioxsardinha@gmail.com>
2021-03-17 14:50:08 +0000

    BFS

--- ai.py
+++ ai.py
@@ -1,6 +1,7 @@
 from tenpair import *
+from time import time
 
-class Node(object):
+'''class Node(object):
     def __init__(self, data, value):
         self.data = data
         self.value = value
@@ -12,9 +13,17 @@ class Node(object):
 class Edge(object):
     def __init__(self, data, node):
         self.data = data
-        self.node = node
+        self.node = node'''
 
+class Node(object):
+    def __init__(self, data):
+        self.data = data
+        self.children = []
+        self.parent = None
 
+    def add_child(self, obj):
+        self.children.append(obj)
+        obj.parent = self
 
 def getPossibleMoves(position,board):
     x = 0
@@ -52,19 +61,7 @@ def getPossibleMoves(position,board):
             
     return moves
 
-def countElements(board):
-    count = 0
-    for line in board:
-        for value in line:
-            if not value == 0:
-                count += 1
-
-    return count
-
-def elementsLeft(board):
-    return (len(board)*len(board[0])) - countElements(board)
-
-def buildTree(board, depth):
+'''def buildTree(board, depth):
     node = Node(board, elementsLeft(board))
 
     if depth == 0:
@@ -85,16 +82,97 @@ def buildTree(board, depth):
                 node.add_edge(edge)
 
     newBoard = [l[:] for l in board]
-    edge = Edge(countElements(newBoard), buildTree(newBoard, depth - 1))
+    edge = Edge(countElements(newBoard), buildTree(deal(newBoard), depth - 1))
     node.add_edge(edge)
 
-    return node
+    return node'''
+
+def countElements(board):
+    count = 0
+    for line in board:
+        for value in line:
+            if not value == 0:
+                count += 1
+
+    return count
+
+def elementsLeft(board):
+    return (len(board)*len(board[0])) - countElements(board)
+
+def getAllMovesInBoard(board):
+    visited = []
+    for i, line in enumerate(board):
+        for j, number in enumerate(line):
+            for move in getPossibleMoves((j,i), board):
+                if ((j,i),move) in visited:
+                    continue
+                visited.append((move,(j,i)))
+                
+    return visited
+
+def getAllBoards(board):
+    boards = []
+    for move,(j,i) in getAllMovesInBoard(board):
+        newBoard = [l[:] for l in board]
+        newBoard = removePiece((j,i), move, newBoard)
+        boards.append(newBoard)
+
+    #newBoard = [l[:] for l in board]
+    #boards.append(deal(newBoard))
+    return boards
+    
+def BFS(board):
+
+    boards = getAllBoards(board)
+    newBoard = [l[:] for l in board]
+    boards.append(deal(newBoard))
+
+    root = Node(board)
+
+    queue = []
+
+    visited = []
+    skip = 0
+    
+    for b in boards:
+        node = Node(b)
+        root.add_child(node)
+        queue.append(node)
+
+    while queue:                
+        node = queue.pop(0)
+
+        visited.append(str(node.data))
+
+        if len(visited) > 99999:
+            showBoard(node.data,0)
+            break
+
+        if win(node.data):
+            print("win")
+            return node
+
+        for b in getAllBoards(node.data):
+            if str(b) in visited:
+                skip += 1
+                continue
+            
+            child = Node(b)
+            node.add_child(child)
+            queue.append(child)
+            visited.append(str(b))
+    
+    
 
 board, moves = initialState()
 
-buildTree(board, 20)
+start = time()
 
-print("Done")
+BFS(board)
+
+end = time()
+
+print("Done in :" + str(end - start) + " seconds")
 
 
 
